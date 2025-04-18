6f8a8893d08624d2c0b03ee8bdfaaaebebdd6b7e
Navamy <Navamy@192.168.1.11>
2025-04-18 16:29:15 +0700

    commit

---
+++ JavaBasics/src/main/java/javabasics/Assignment1.java
@@ -0,0 +1,9 @@
+package javabasics;
+
+public class Assignment1 {
+	public static void main(String[] args) {
+		System.out.println("name - Navamy");
+		System.out.println("age - 29");
+		System.out.println("place - Vakkom");
+	}
+}
