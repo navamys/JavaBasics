e789537b3def1aa751d23a174203f94c09b801f0
Navamy <Navamy@Akhil-PC>
2025-04-21 13:50:46 +0530

    commit

---
+++ JavaBasics/src/main/java/javabasics/Assignment5c.java
@@ -0,0 +1,31 @@
+package javabasics;
+
+public class Assignment5c {
+
+	public static void main(String[] args) {
+		// find out vowel
+		char letter='u';
+		switch(letter) {
+			case 'a':
+				System.out.println("a is vowel");
+				break;
+			case 'e':
+				System.out.println("e is vowel");
+				break;
+			case 'i':
+				System.out.println("i is vowel");
+				break;
+			case 'o':
+				System.out.println("o is vowel");
+				break;
+			case 'u':
+				System.out.println("u is vowel");
+				break;
+			default:
+				System.out.println(letter+ " is not a vowel");
+		}
+		
+
+	}
+
+}
